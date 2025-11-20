7dd9adc7e1cccbb521c94f556a1c7b3e8af2145f
Chris Barrett <chris@walrus.cool>
2025-11-20 20:10:30 +1300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Incremental loading for Claude Code skills";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            actionlint
+            deno
+            pre-commit
+            shellcheck
+          ];
+        };
+      };
+    };
+}
